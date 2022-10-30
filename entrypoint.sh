d17efac6e0f2ad10006b3cf726ff5d321951dacb
Rahammetoela Toekiman <fuseteam@duck.com>
2022-10-29 23:05:00 -0300

    can i see the docker command?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+set -x
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:"/tmp/clickable_base"  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
