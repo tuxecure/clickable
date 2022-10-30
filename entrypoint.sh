53dd368cdf8c79cd7b264c29e5531eb20bc65a57
Linas Daneliukas <linas@daneliukas.eu>
2022-10-30 00:27:30 -0300

    does this mount it correctly?

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 ls
 pwd
 set -x
-exec docker run --workdir "/tmp/clickable_base" -v "/var/run/docker.sock":"/var/run/docker.sock" -v "$PWD":"/tmp/clickable_base"  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+exec docker run --workdir "/tmp/clickable_base" -v "/var/run/docker.sock":"/var/run/docker.sock" -v "$RUNNER_WORKSPACE":"/tmp/clickable_base"  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
