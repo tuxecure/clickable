a3b5c673e111c8063ff7d917e996be6bbf3b9192
Rahammetoela Toekiman <fuseteam@duck.com>
2022-10-29 23:36:14 -0300

    show working directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+pwd
 set -x
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:"/tmp/clickable_base"  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
