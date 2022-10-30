978873247c3de8cc8d98bf9da4582e3654aefeed
Rahammetoela Toekiman <fuseteam@duck.com>
2022-10-30 11:53:05 -0300

    grep environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+env | egrep -v "^(INPUT_*|GITHUB_*|ACTIONS_*|RUNNER_*|HOME|CI)" | awk '$1 ~ /^\w+=/' | xargs -0
+
 exec docker run --workdir "$INPUT_WORKSPACE" -v "/var/run/docker.sock":"/var/run/docker.sock" -v "$RUNNER_WORKSPACE":"/tmp/clickable_base"  --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
