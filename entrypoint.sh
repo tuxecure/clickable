b7f3b08b031fef33c0fd167a694d34429f213fc9
Rahammetoela Toekiman <fuseteam@duck.com>
2022-10-30 00:53:47 -0300

    yay clean up time

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-ls
-pwd
-set -x
 exec docker run --workdir "$INPUT_WORKSPACE" -v "/var/run/docker.sock":"/var/run/docker.sock" -v "$RUNNER_WORKSPACE":"/tmp/clickable_base"  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
