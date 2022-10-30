4b658b367c7f32f7746960fca896656f611e3bca
Rahammetoela Toekiman <fuseteam@duck.com>
2022-10-29 23:47:53 -0300

    show contents

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+ls
 pwd
 set -x
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:"/tmp/clickable_base"  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
