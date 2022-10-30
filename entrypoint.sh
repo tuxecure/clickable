0dc6a6b9d716d6887e932da21ae40d2eed2cce74
Rahammetoela Toekiman <fuseteam@duck.com>
2022-10-30 01:11:39 -0300

    use bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec docker run --workdir "$INPUT_WORKSPACE" -v "/var/run/docker.sock":"/var/run/docker.sock" -v "$RUNNER_WORKSPACE":"/tmp/clickable_base"  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+exec docker run --workdir "$INPUT_WORKSPACE" -v "/var/run/docker.sock":"/var/run/docker.sock" -v "$RUNNER_WORKSPACE":"/tmp/clickable_base"  --entrypoint=bash $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
