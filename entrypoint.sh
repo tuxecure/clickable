fdd89eed98849d5a714635d769f9c35c79791665
Rahammetoela Toekiman <fusekai@outlook.com>
2022-11-02 09:35:30 -0300

    is this the culprit?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 env | egrep -v "^(#|;| |PATH|SHLVL|HOSTNAME|DOCKER_*)" | awk '$1 ~ /^\w+=/' | xargs -0 > docker-run-action.env
-set -x
 exec docker run --env-file docker-run-action.env --workdir "$INPUT_WORKSPACE" -v "/var/run/docker.sock":"/var/run/docker.sock" -v "$RUNNER_WORKSPACE":"$RUNNER_WORKSPACE"  --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
